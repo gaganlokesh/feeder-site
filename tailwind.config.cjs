76b148d064ee3aeb24ce8043129d39e4688d7cd5
Gagan Lokesh <gaganlokesh@gmail.com>
2022-06-02 17:15:13 +0530

    Add custom color palette and container padding scale for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      primary: colors.indigo,
+      gray: colors.gray,
+      white: colors.white
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '4rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
 }
